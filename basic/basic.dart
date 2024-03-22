7fb9e70ccccf2c1e5bbcafe44ca9ec8b42221cc9
Mohamed Ali <mo7amedaliebaid@gmail.com>
2024-03-22 14:39:03 +0200

    linked_list

--- basic/basic.dart
+++ basic/basic.dart
@@ -1,7 +1,6 @@
 import 'dart:collection';
 
-void main(){
-
+void main() {
   final people = ['Pablo', 'Manda', 'Megan'];
   print(people[1]);
   people.add('Edith');
@@ -9,14 +8,32 @@ void main(){
   people.insert(0, 'Ray');
   print(people);
 
+  List<String> searchedForPeople =
+      people.where((element) => element.toLowerCase().contains("b")).toList();
+  print(searchedForPeople);
+
   final scores = {'Eric': 9, 'Mark': 12, 'Wayne': 1};
   scores['Andrew'] = 0;
   print(scores);
-
+  final goodScores = scores.values.where((value) => value > 8);
+  print(goodScores);
   final hashMap = HashMap.of(scores);
   print(hashMap);
 
   var bag = {'Candy', 'Juice', 'Gummy'};
   bag.add('Candy');
   print(bag);
+
+  final peopleList = ['Pablo', 'Manda', 'Megan', 'Megan'];
+  final peopleSet = peopleList.toSet();
+  print(peopleSet);
+
+  String name='Mohamed Ali';
+  print(name.length.toString());
+
+  List<String> nameParts=name.split(' ');
+  String fName=nameParts.first;
+  String lName=nameParts.last;
+  print(fName);
+  print(lName);
 }
---
+++ linked_list/challenge.dart
@@ -0,0 +1,16 @@
+void main() {
+  String sentence = "   fly me   to   the moon  ";
+///  String firstWord = getFirstWord(sentence);
+//  print(firstWord); // Output: fly
+}
+
+int lengthOfLastWord(String s) {
+  // Trim leading and trailing whitespace
+  s = s.trim();
+
+  // Split the sentence by whitespace
+  List<String> words = s.split(RegExp(r'\s+'));
+
+  // Return the first word
+  return words.last.length;
+}
