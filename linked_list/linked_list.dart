45b499c4d535d5344e671aec032ca704bb9854a8
Mohamed Ali <mo7amedaliebaid@gmail.com>
2024-03-24 01:34:08 +0200

    linked_list

--- linked_list/linked_list.dart
+++ linked_list/linked_list.dart
@@ -24,6 +24,51 @@ class LinkedList<E> {
 
   bool get isEmpty => head == null;
 
+  void push(E value) {
+    head = Node(value: value, next: head);
+    tail ??= head;
+  }
+
+  void append(E value) {
+// 1
+    if (isEmpty) {
+      push(value);
+      return;
+    }
+// 2
+    tail!.next = Node(value: value);
+
+// 3
+    tail = tail!.next;
+  }
+
+  Node<E>? nodeAt(int index) {
+// 1
+    var currentNode = head;
+    var currentIndex = 0;
+
+// 2
+    while (currentNode != null && currentIndex < index) {
+      currentNode = currentNode.next;
+      currentIndex += 1;
+
+      return currentNode;
+    }
+    return null;
+  }
+
+  Node<E> insertAfter(Node<E> node, E value) {
+// 1
+    if (tail == node) {
+      append(value);
+      return tail!;
+    }
+
+// 2
+    node.next = Node(value: value, next: node.next);
+    return node.next!;
+  }
+
   @override
   String toString() {
     if (isEmpty) return 'Empty list';
@@ -37,9 +82,29 @@ void main() {
   final node3 = Node(value: 3);
   node1.next = node2;
   node2.next = node3;
+  // You know that the list is empty if the head is null . Also, since you already designed
+  // Node to recursively print any nodes that follow it, you can print the entire linked list
+  // just by calling head.toString
   print(node1);
   final linkedList = LinkedList(node1, node3);
   final linkedList1 = LinkedList(null, null);
   print(linkedList);
   print(linkedList1);
+  final list = LinkedList<int>(null, null);
+  list.push(3);
+  list.push(2);
+  list.push(1);
+  print(list);
+  list.append(1);
+  list.append(2);
+  list.append(3);
+  print(list);
+  final anotherList = LinkedList<int>(null, null);
+  anotherList.push(4);
+  anotherList.push(5);
+  anotherList.push(6);
+  print('Before: $anotherList');
+  var middleNode = anotherList.nodeAt(1)!;
+  anotherList.insertAfter(middleNode, 42);
+  print('After:$anotherList');
 }
--- linked_list/linked_list.md
+++ linked_list/linked_list.md
@@ -4,7 +4,8 @@
    <img src="https://github.com/mo7amedaliEbaid/ds_dart/blob/129c2fd7ce2782ac4604576d69ac77edcc92df30/assets/linked_list.png"  alt="linked list"/>
 </p>
 
-- A linked list is a collection of values arranged in a linear, unidirectional sequence. It has several theoretical advantages over contiguous storage options such as the Dart List :
+- A linked list is a collection of values arranged in a linear, unidirectional sequence. 
+- It has several theoretical advantages over contiguous storage options such as the Dart List :
  - Constant time insertion and removal from the front of the list.
  - Reliable performance characteristics.
 - A linked list is a chain of nodes:
@@ -12,3 +13,10 @@
 1. Hold a value.
 2. Hold a reference to the next node. A null reference indicates the end of the list.
 - A linked list has the concept of a head and tail, which refers to the first and last nodes of the list respectively.
+
+## Adding Values to a List.
+- There are three ways to add values to a linked list, each having its own unique performance characteristics:
+  - push : Adds a value at the front of the list `head-first insertion`.
+  - append : Adds a value at the end of the list `tail-end insertion`.
+  - insertAfter : Adds a value after a particular node in the list.
+
